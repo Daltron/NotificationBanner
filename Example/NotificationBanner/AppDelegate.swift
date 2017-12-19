9a4c623808a5ad5c7fe544e421e09f6a6ff73639
Dalton <daltonhint4@gmail.com>
2017-12-19 09:31:51 -0600

    Release v1.5.3

--- Example/NotificationBanner/AppDelegate.swift
+++ Example/NotificationBanner/AppDelegate.swift
@@ -16,7 +16,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplicationLaunchOptionsKey: Any]?) -> Bool {
         
         let navigationController = UINavigationController(rootViewController: ExampleViewController())
-        //navigationController.setNavigationBarHidden(true, animated: false)
         let tabBarController = UITabBarController()
         tabBarController.viewControllers = [navigationController]
         
