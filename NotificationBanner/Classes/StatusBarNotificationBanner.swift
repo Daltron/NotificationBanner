bceb48676b6c492e2b603fc7c5cb38c8aca336ce
Muhammad Rifqi Fatchurrahman <muh_rif@live.com>
2018-10-03 01:56:39 +0700

    add custom view for StatusBarNotificationBanner

--- NotificationBanner/Classes/StatusBarNotificationBanner.swift
+++ NotificationBanner/Classes/StatusBarNotificationBanner.swift
@@ -1,19 +1,19 @@
 /*
- 
+
  The MIT License (MIT)
  Copyright (c) 2017-2018 Dalton Hinterscher
- 
+
  Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"),
  to deal in the Software without restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense,
  and/or sell copies of the Software, and to permit persons to whom the Software is furnished to do so, subject to the following conditions:
- 
+
  The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
- 
+
  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
  MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR
  ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN CONNECTION WITH
  THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
- 
+
  */
 
 import UIKit
@@ -25,7 +25,7 @@ import UIKit
 #endif
 
 public class StatusBarNotificationBanner: BaseNotificationBanner {
-    
+
     public override var bannerHeight: CGFloat {
         get {
             if let customBannerHeight = customBannerHeight {
@@ -39,7 +39,7 @@ public class StatusBarNotificationBanner: BaseNotificationBanner {
             customBannerHeight = newValue
         }
     }
-    
+
     override init(style: BannerStyle, colors: BannerColorsProtocol? = nil) {
         super.init(style: style, colors: colors)
 
@@ -50,7 +50,7 @@ public class StatusBarNotificationBanner: BaseNotificationBanner {
         titleLabel!.textAlignment = .center
         titleLabel!.textColor = .white
         contentView.addSubview(titleLabel!)
-        
+
         titleLabel!.snp.makeConstraints { (make) in
             make.top.equalToSuperview()
             make.left.equalToSuperview().offset(5)
@@ -60,21 +60,31 @@ public class StatusBarNotificationBanner: BaseNotificationBanner {
 
         updateMarqueeLabelsDurations()
     }
-    
+
     public convenience init(title: String,
                             style: BannerStyle = .info,
                             colors: BannerColorsProtocol? = nil) {
         self.init(style: style, colors: colors)
         titleLabel!.text = title
     }
-    
+
     public convenience init(attributedTitle: NSAttributedString,
                             style: BannerStyle = .info,
                             colors: BannerColorsProtocol? = nil) {
         self.init(style: style, colors: colors)
         titleLabel!.attributedText = attributedTitle
     }
-    
+
+    public init(customView: UIView) {
+        super.init(style: .none)
+        contentView.addSubview(customView)
+        customView.snp.makeConstraints { make in
+            make.edges.equalTo(contentView)
+        }
+
+        spacerView.backgroundColor = customView.backgroundColor
+    }
+
     required public init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
     }
