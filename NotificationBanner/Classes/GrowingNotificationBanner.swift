1165345e1b25b1330b0d924a16657fdd29a68e06
Roland Kákonyi <roland@getmimo.com>
2019-10-04 16:39:13 +0200

    fix overlapping with status bar on iOS 13 for non-notch devices

--- NotificationBanner/Classes/GrowingNotificationBanner.swift
+++ NotificationBanner/Classes/GrowingNotificationBanner.swift
@@ -70,7 +70,7 @@ open class GrowingNotificationBanner: BaseNotificationBanner {
                     actualBannerHeight += innerSpacing
                 }
                 
-                return max(actualBannerHeight, minHeight)
+                return statusBarHeightAdjustment + max(actualBannerHeight, minHeight)
             }
         } set {
             customBannerHeight = newValue
@@ -180,6 +180,19 @@ open class GrowingNotificationBanner: BaseNotificationBanner {
     
     required public init?(coder aDecoder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+
+
+    private var statusBarHeightAdjustment: CGFloat {
+        guard #available(iOS 13.0, *), !NotificationBannerUtilities.isNotchFeaturedIPhone() else {
+            return 0
+        }
+
+        return UIApplication.shared.statusBarFrame.height
+    }
+
+    override func spacerViewHeight() -> CGFloat {
+        return super.spacerViewHeight() + statusBarHeightAdjustment
     }
 }
 
