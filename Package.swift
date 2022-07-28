2674ea656d63a934d9c8c406dbe0e0d6c763e8ef
ShenYj <shenyanjie123@foxmail.com>
2022-07-28 13:07:30 +0800

    update spm config

--- Package.swift
+++ Package.swift
@@ -10,7 +10,7 @@ let package = Package(
         .library(name: "NotificationBannerSwift", targets: ["NotificationBannerSwift"])
     ],
     dependencies: [
-        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.0.1"),
+        .package(url: "https://github.com/SnapKit/SnapKit", from: "5.0"),
         .package(url: "https://github.com/cbpowell/MarqueeLabel", from: "4.0.1")
     ],
     targets: [
