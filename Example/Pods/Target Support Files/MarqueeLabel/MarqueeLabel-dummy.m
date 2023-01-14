e3e99f4d4220f0706af6cb128e0b57874cfef6f3
daltron <daltonhint4@gmail.com>
2023-01-14 13:33:11 -0600

    Removed CocoaPods from example project

--- Example/Pods/Target Support Files/MarqueeLabel/MarqueeLabel-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_MarqueeLabel : NSObject
-@end
-@implementation PodsDummy_MarqueeLabel
-@end
--- Example/Pods/Target Support Files/NotificationBannerSwift/NotificationBannerSwift-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_NotificationBannerSwift : NSObject
-@end
-@implementation PodsDummy_NotificationBannerSwift
-@end
